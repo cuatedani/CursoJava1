aa065fc5863c2e39f7a41f14f21567153c8d2eff
cuatedani <cuate.danielito@gmail.com>
2026-02-05 12:31:02 -0700

    Init Curso Java

--- src/platzi/play/main.java
+++ src/platzi/play/main.java
@@ -1,7 +1,14 @@
 package platzi.play;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args){
         System.out.println("Hola Mundo en Java");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Cual es tu nombre?: ");
+        String nombre = scan.nextLine();
+        System.out.println("Saludos ingeniero " + nombre + " :)");
     }
 }
